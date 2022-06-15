d6dae8b5f3bdd9d38ea981a151b9ad299253edd4
yirman <germanguevara2496@gmail.com>
2022-06-14 22:58:10 -0400

    basis for main views

---
+++ app/src/main/java/com/german/eshop/customer/ui/MainActivity.kt
@@ -0,0 +1,61 @@
+package com.german.eshop.customer.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupWithNavController
+import com.german.eshop.customer.R
+import com.german.eshop.customer.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var appBarConfiguration: AppBarConfiguration
+
+    private lateinit var navController : NavController
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setSupportActionBar(binding.toolbar)
+        setupNavigationController()
+        setupUI()
+    }
+
+    private fun setupUI(){
+        binding.navView
+            .getHeaderView(0)
+            .findViewById<TextView>(R.id.title).text =
+            "${getString(R.string.app_name).toCharArray()[0]}!"
+        binding.navView
+            .getHeaderView(0)
+            .findViewById<TextView>(R.id.message).text = getString(R.string.welcome)
+    }
+
+    private fun setupNavigationController() {
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.navController
+        val appBarConfiguration = AppBarConfiguration(navController.graph, binding.drawerLayout)
+        binding.toolbar.setupWithNavController(navController, appBarConfiguration)
+        binding.navView.setupWithNavController(navController)
+//        binding.navView.menu.findItem(R.id.nav_logout).setOnMenuItemClickListener {
+////            firebaseAuth.currentUser.providerId   SABER EN CUAL MIERDA ESTÁ LOGUEADO
+//            LoginManager.getInstance().logOut();
+//            googleSignInClient?.signOut()?.addOnCompleteListener {
+//                if(it.isSuccessful){
+//                    firebaseAuth?.signOut()
+//                    navController.navigate(R.id.action_homeFragment_to_loginActivity)
+//                    finish()
+//                }
+//            }
+//            true
+//        }
+    }
+
+    override fun onSupportNavigateUp(): Boolean = NavigationUI.navigateUp(navController, binding.drawerLayout)
+}
